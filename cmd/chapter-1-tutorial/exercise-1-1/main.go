db70f4505d70ae25f3aee7de9b08734127bb9b1f
Alois Carrera <aloiscarrera@gmail.com>
2021-03-07 17:43:30 -0500

    feat: :sparkles: Exercise 1.1 completed

---
+++ cmd/chapter-1-tutorial/exercise-1-1/main.go
@@ -0,0 +1,16 @@
+/*
+ Modify the echo program to also allow print os.Args[0], the name of the command that invoked it.
+*/
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+
+	fmt.Println("all args ", strings.Join(os.Args[:], " "))
+
+}
